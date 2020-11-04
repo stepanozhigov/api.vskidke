7efeacab5fd465b85f82e21a783ab5d11615d2be
stepanozhigov <stepan.ozhigov@gmail.com>
2020-11-04 21:53:27 +0200

    employee factory

--- database/factories/EmployeeFactory.php
+++ database/factories/EmployeeFactory.php
@@ -22,11 +22,11 @@ class EmployeeFactory extends Factory
     public function definition()
     {
         return [
-            'first_name' => $this->firstName($gender = null|'male'|'female'),
-            'last_name' => $this->lastName,
-            'birthday' =>$this->dateTimeBetween('-50 years', '-20 years'),
-            'email' => $this->unique()->safeEmail,
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'first_name' => $this->faker->firstName,
+            'last_name' => $this->faker->lastName,
+            'birthday' =>$this->faker->dateTimeBetween('-50 years', '-20 years'),
+            'email' => $this->faker->unique()->safeEmail,
+            'phone' =>$this->faker->phoneNumber
         ];
     }
 }
