b6c0d53420c2af0ace7c33955e14b99d0cb1e1b0
stepanozhigov <stepan.ozhigov@gmail.com>
2020-11-05 17:19:09 +0200

    route clean

--- routes/api.php
+++ routes/api.php
@@ -18,10 +18,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::post('/register',[AuthController::class,'register']);
 Route::post('/login',[AuthController::class,'login']);
 
@@ -31,9 +27,5 @@ Route::middleware('auth:sanctum')->group(function() {
         Route::apiResource('employees', EmployeeController::class);
         Route::apiResource('projects', ProjectController::class);
 });
-
-        // Route::apiResource('users', UserController::class);
-        // Route::apiResource('employees', EmployeeController::class);
-        // Route::apiResource('projects', ProjectController::class);
 
     
