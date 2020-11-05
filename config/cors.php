1fda7bccb0c3a3765a6aa2499b14a471b598cb08
stepanozhigov <stepan.ozhigov@gmail.com>
2020-11-06 00:15:46 +0200

    cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:8080'],
+    'allowed_origins' => ['http://localhost:8080','https://app.vskidke.ru'],
 
     'allowed_origins_patterns' => [],
 
