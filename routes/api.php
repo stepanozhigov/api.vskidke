2198bad2937638979fa016f6e8557bc4639744f8
stepanozhigov <stepan.ozhigov@gmail.com>
2020-11-08 00:38:08 +0200

    user route

--- routes/api.php
+++ routes/api.php
@@ -22,8 +22,11 @@ Route::post('/register',[AuthController::class,'register']);
 Route::post('/login',[AuthController::class,'login']);
 
 Route::middleware('auth:sanctum')->group(function() {
+        Route::get('user',function() {
+                return auth()->user();
+        });
         Route::post('/logout',[AuthController::class,'logout']);
-        Route::apiResource('users', UserController::class);
+        Route::apiResource('users', UserController::class); 
         Route::apiResource('employees', EmployeeController::class);
         Route::apiResource('projects', ProjectController::class);
 });
